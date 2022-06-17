919ce4e39e2877e84cd500f84d8b18997189fa53
filo-san14 <fatimazahraeacharki14@gmail.com>
2022-06-17 15:37:18 +0100

    still want to change directory

---
+++ src/home.php
@@ -0,0 +1,180 @@
+
+<!-- NEW CODE -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    @font-face {
+      font-family: "MyFont";
+      src: url("BillionMiracles.otf");
+    }
+  </style>
+  <link rel="stylesheet" href="style.css">
+  <title>Home</title>
+</head>
+<body>
+  <!-- nav -->
+  <nav>
+    <img src="loginRegister/assets/painto logo 1.png" alt="">
+    <ul>
+      <a href="/loginRegister/src/index.html"><li>Home</li></a>
+      <a href=""><li>servises</li></a>
+      <a href=""><li>painters</li></a>
+      <a href=""><li>contact</li></a>
+    </ul>
+    <input type="button" value="Login" id="login">
+  </nav>
+  <div class="rect" id="green"></div>
+  <div class="rect" id="blue"></div>    
+  
+  <!-- nav end -->
+    <header>
+      <img src="../dist/living-room-interior-wall-mockup-with-pink-sofa-on-SN3F9Y6 1.png" alt="">
+    </header>
+    <main>
+      <div id="intro-left">
+        <h5>ABOUT COMPANY</h5>
+        <h2>We’re Providing Quality Wall Designs</h2>
+        <div id="line"></div>
+        <p>We have 12+ years of experiences for give you better quality results.</p>
+        <div class="expertise-part">
+          <img src="../dist/t (1).png">
+          <p>Smart & Unique Wall Work</p>
+        </div>
+        <div class="expertise-part">
+          <img src="../dist/warranty.png">
+          <p>The Best Quality Standards</p>
+        </div>
+        <button>Read More About</button>
+      </div>
+      <div id="intro-right">
+        <img src="../dist/9 simple wall paint ideas that will transform your interior on a budget.png">
+        <div></div>
+      </div>
+    </main>
+
+    <section id="main-2">
+
+      <div>
+        <img src="../dist/mind.png" alt="">
+        <h3>Smart work</h3>
+        <p>We guarantee the best materials for every single project. We follow the latest trends and technology.</p>
+      </div>
+
+      <div>
+        <img src="../dist/wallpaper1.png" alt="">
+        <h3>Unique designs</h3>
+        <p>The word “painters” is in our title and as such we give you the right expertise for every project.</p>
+      </div>
+
+      <div>
+        <img src="../dist/programmer.png" alt="">
+        <h3>Skilled team</h3>
+        <p>We hire the most experienced personnel to fulfill all the required projects that come to us.</p>
+      </div>
+
+      <div>
+        <img src="../dist/icons8-tags-64.png" alt="">
+        <h3>Best pricing</h3>
+        <p>Quality work does not always mean “expensive”; we are always open to hear what your budget is.</p>
+      </div>
+
+    </section>
+    
+    <!-- Meet our team part -->
+
+    <section id="meet-our-team">
+
+      <h1>Meet our team</h1>
+
+      <p>Our team of painting contractors can create everything from scratch and has ingenious ideas is here and is more than happy to help you.</p>
+
+      <div id="team">
+          <img src="../img/bogos.png">
+          <img src="../img/mdm.png">
+          <img src="../img/3azi.png">
+      </div>
+
+      <input type="button" value="View Our Team">
+    </section>
+    <section id="warrenty-part">
+      <div id="warrenty">
+        <p>Trusted by More then 800 Most Popular Brands</p>
+        <img src="../dist/brand-1-4.png">
+        <img src="../dist/brand-1-5.png">
+        <img src="../dist/brand-1-2.png">
+        <img src="../dist/brand-1-3.png">
+      </div>
+
+      <div id="last-part">
+        <p> News & Services</p>
+        <h3>SERVICES & UPDATES</h3>
+      </div>
+
+    </section>
+    <section id="services">
+
+      <div>
+        <img src="../img/simple.png">
+        <img src="../img/design.png">
+        <img src="../img/wallpaper.png">
+      </div>
+      <p>
+        Life is better in color! <br>
+        Your professional painting and coating service provider for Tangier ☻.
+      </p>
+    </section>
+    <div id="Subscribe">
+      <div id="sub-background"></div>
+      <div id="sub-text">
+        <p id="h3-1">Join Our Newsletter</p>
+        <p id="h3-2">and Stay Updated!</p>
+      </div>
+
+      <div id="sub-input">
+        <input type="text" placeholder="Enter your email">
+        <input type="button" value="Subscribe">
+      </div>
+    </div>
+
+
+    <!-- footer -->
+
+
+    <footer>
+      <div id="footer-content">
+        <div>
+          <img src="../dist/painto logo.png">
+        </div>
+
+        <div>
+          <p>Follow Us</p>
+          <div id="social-media">
+            <img src="../dist/facebook.png">
+            <img src="../dist/youtube.png">
+            <img src="../dist/twitter.png">
+            <img src="../dist/instagram.png">
+          </div>
+        </div>
+
+        <div id="support">
+          <p>Support</p>
+          <ul>
+            <li>FAQs</li>
+            <li>Privacy Policy</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="copyright">
+        <p>Copyright © 2022 Painto Project by Filo-san</p>
+      </div>
+    </footer>
+
+
+</body>
+</html>
